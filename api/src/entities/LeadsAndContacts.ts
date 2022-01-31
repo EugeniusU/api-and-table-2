8875a9a9013f88aead449c1dd447504ad901bd2b
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2022-01-31 15:58:01 +0300

    Update LeadsAndContacts.ts

--- api/src/entities/LeadsAndContacts.ts
+++ api/src/entities/LeadsAndContacts.ts
@@ -1,24 +1,15 @@
 import { Column, Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm"
-import { Contacts } from "./Contacts"
-import { Leads } from "./Leads"
+//import { Contacts } from "./Contacts"
+//import { Leads } from "./Leads"
 
 @Entity()
 export class LeadsAndContacts {
     @PrimaryGeneratedColumn()
     id: number
 
-//    @ManyToMany(() => Leads, (lead: Leads) => lead.externalId)
-//    public leadId: Leads[]
+    @PrimaryColumn({unique: true})
+    leadId: number
 
-/*    @ManyToMany(() => Contacts, (contact: Contacts) => contact.leads)
-    @JoinTable()
-    public leads: Contacts[]*/
-
-    @Column('jsonb')
-///    public leadsId: number[]
-    leadsId: string
-
-    @Column('jsonb')
-///    public contactsId: number[]
-    contactsId: string
+    @PrimaryColumn()
+    contactId: number
 }
