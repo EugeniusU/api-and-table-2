cba7ea8ba8e9781fb105f2ab13e390a1109f5176
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2022-01-31 11:44:40 +0300

    Add files via upload

---
+++ page/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+      <v-main-wrapper/>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from '@vue/composition-api'
+import VMainWrapper from './components/v-main-wrapper.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VMainWrapper
+  }
+})
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+
+
+</style>
